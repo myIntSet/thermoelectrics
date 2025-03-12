059ae7d590596f5036d61aae6a5c5917edd00ea2
Agnes Hessman <ag3830he@fys.local>
2025-03-12 11:05:30 +0100

    Working version

--- plotting_machine.py
+++ plotting_machine.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
 
 
 def plot_all(epsilons, VBs, I, I_var, P, J_QH, sigma, TUR):
@@ -29,25 +30,53 @@ def plot_all(epsilons, VBs, I, I_var, P, J_QH, sigma, TUR):
     fig.suptitle("Zoomed in region", fontsize=16, fontweight='bold')
     plt.show()
 
-    '''
-    # Plot on different subplots using the axes array
-    img = axes[0,0].imshow(I, extent=[epsilons[0], epsilons[-1], VBs[0], VBs[-1]], aspect='auto', origin='lower')
-    fig.colorbar(img, ax=axes[0,0]) 
-    axes[0,0].set_title("Current")
-    axes[0,0].set_xlabel('epsilon')
-    axes[0,0].set_ylabel('V')
-    #img.set_clim(-0.1, 0.1)  # Set color limits
-
-    img = axes[0,1].imshow(I_var, extent=[epsilons[0], epsilons[-1], VBs[0], VBs[-1]], aspect='auto', origin='lower')
-    fig.colorbar(img, ax=axes[0,1]) 
-    axes[0,1].set_title("Noise")
-    axes[0,1].set_xlabel('epsilon')
-    axes[0,1].set_ylabel('V')
-
-    fig.suptitle("CZoomed in region", fontsize=16, fontweight='bold')
-    # Adjust layout to prevent overlap
-    #plt.tight_layout(rect=[0, 0, 1, 0.95])  # Leaves space for suptitle
-    '''
-
 def plot_one(Y):
     pass
+
+def plot_zoomed_out(epsilons, VBs, Y1, title1, Y2, title2, epsilons_zoom, VBs_zoom, Y1_zoom, Y2_zoom, suptitle):
+
+    # Create Figure
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(16, 5))
+    print(VBs[-1])
+
+    # Main imshow plot
+    img = axes[0].imshow(Y1, extent=[epsilons[0], epsilons[-1], VBs[0], VBs[-1]], aspect='auto', origin='lower')
+    fig.colorbar(img, ax=axes[0])
+    axes[0].set_title(title1)
+    axes[0].set_xlabel(r"$\epsilon$")
+    axes[0].set_ylabel('$V_B$')
+
+    # Define zoom area (e.g., focus on a small square region)
+    eps_min, eps_max = epsilons_zoom[0], epsilons_zoom[-1]   #0, 2.1  # Epsilon range
+    v_min, v_max = VBs_zoom[0], VBs_zoom[-1] #-0.5, 0.5  # V range
+
+    inset_width = 0.3  # 30% of the parent axes
+    inset_height = inset_width * (v_max - v_min) / (eps_max - eps_min)  # Maintain aspect ratio
+    axins = inset_axes(axes[0], width=f"{inset_width*100}%", height=f"{inset_height*100}%", loc="upper right")
+    img_zoom = axins.imshow(Y1_zoom, extent=[eps_min, eps_max, v_min, v_max], aspect='auto', origin='lower')
+    axins.set_xticks([])  # Remove ticks for clarity
+    axins.set_yticks([])
+
+    # Mark the zoomed region with a rectangle
+    axes[0].plot([eps_min, eps_max, eps_max, eps_min, eps_min], [v_min, v_min, v_max, v_max, v_min], 'r-', linewidth=2)
+    # Connect main plot and inset with lines
+    mark_inset(axes[0], axins, loc1=2, loc2=4, fc="none", ec="red")
+
+    img = axes[1].imshow(Y2, extent=[epsilons[0], epsilons[-1], VBs[0], VBs[-1]], aspect='auto', origin='lower')
+    fig.colorbar(img, ax=axes[1]) 
+    axes[1].set_title(title2)
+    axes[1].set_xlabel(r"$\epsilon$")
+    axes[1].set_ylabel('$V_B$')
+
+    inset_width = 0.3  # 30% of the parent axes
+    inset_height = inset_width * (v_max - v_min) / (eps_max - eps_min)  # Maintain aspect ratio
+    axins = inset_axes(axes[1], width=f"{inset_width*100}%", height=f"{inset_height*100}%", loc="upper right")
+    img_zoom = axins.imshow(Y2_zoom, extent=[eps_min, eps_max, v_min, v_max], aspect='auto', origin='lower')
+
+    axins.set_xticks([])  # Remove ticks for clarity
+    axins.set_yticks([])
+    axes[1].plot([eps_min, eps_max, eps_max, eps_min, eps_min], [v_min, v_min, v_max, v_max, v_min], 'r-', linewidth=2)
+    mark_inset(axes[1], axins, loc1=2, loc2=4, fc="none", ec="red")
+
+    fig.suptitle(suptitle, fontsize=16, fontweight='bold')
+    plt.show()
