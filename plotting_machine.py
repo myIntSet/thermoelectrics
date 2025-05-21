ffeccb467f43047d0de45eef83b5fc9aaebe0c74
Agnes Hessman <ag3830he@fys.local>
2025-05-21 19:17:30 +0200

    diverse

--- plotting_machine.py
+++ plotting_machine.py
@@ -3,16 +3,23 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
 
 
-def plot_single_report(plt, epsilons, VBs, Y, title):
+def plot_single_report(plt, epsilons, VBs, Y, title, cmap, vmin=None, vmax=None, plotting_TURs=False):
     text_font = 35
     plt.rcParams.update({'font.size': 20})  # Applies globally
     plt.figure(figsize=(8, 5))  # Optional: set figure size
-    if title == r'$\Phi$ < 2':
+    if title == r'$\Phi$ < 2' or plotting_TURs:
         Y = np.ma.masked_where(Y >= 2, Y)
     elif title == 'P > 0':
         Y = np.ma.masked_where(Y <= 0, Y)
-    img = plt.imshow(Y, extent=[epsilons[0], epsilons[-1], VBs[0], VBs[-1]], aspect='auto', origin='lower')
-    plt.colorbar(img)
+    img = plt.imshow(Y, extent=[epsilons[0], epsilons[-1], VBs[0], VBs[-1]], aspect='auto', origin='lower', cmap=cmap, vmin=vmin, vmax=vmax)
+    cbar = plt.colorbar(img)
+    if title == r'$J_{QH}$':
+        #lamda0:
+        #cbar.set_ticks([0.0015, 0.0010, 0.0005, 0.0000, -0.0005, -0.0010, -0.0015])
+        #cbar.set_ticklabels(['> 0.0015', '0.0010', '0.0005', '0.0000', '-0.0005', '-0.0010', '< -0.0015'])
+        #lamda1:
+        cbar.set_ticks([0.005, 0.004, 0.002, 0.00, -0.002, -0.004, -0.005])
+        cbar.set_ticklabels(['> 0.005', '0.004', '0.002', '0.00', '-0.002', '-0.004', '< -0.005'])
     actual_vmin, actual_vmax = img.get_clim()
     print('actual_vmin,', actual_vmin, actual_vmax)
     plt.title(title, fontsize=text_font)
@@ -45,6 +52,73 @@ def plot_all(epsilons, VBs, I, I_var, P, J_QH, sigma, TUR, title):
 
     plt.tight_layout(rect=[0, 0, 1, 0.95])
     fig.suptitle(title, fontsize=16, fontweight='bold')
+    plt.show()
+
+def plot_big_coulomb_report_cropped(epsilons, VBs, Y, title, epsilons_zoom, VBs_zoom, Y_zoom):
+
+    # Create Figure
+
+    #plt.figure(figsize=(8, 5))  # Optional: set figure size
+    text_font = 16
+    #plt.rcParams.update({'font.size': 16})  # Applies globally
+    fig, ax = plt.subplots(figsize=(12, 4))
+
+    # Main imshow plot
+    img = ax.imshow(Y, extent=[epsilons[0], epsilons[-1], VBs[49], VBs[149]], aspect='auto', origin='lower', cmap='bwr')
+    #ax.scatter(epsilons[10], VBs[50])
+    fig.colorbar(img, ax=ax)
+    ax.set_title(title, fontsize=text_font)
+    ax.set_xlabel(r"$\epsilon$", fontsize=text_font)
+    ax.set_ylabel('$V_B$', fontsize=text_font)
+
+    # Define zoom area (e.g., focus on a small square region)
+    eps_min, eps_max = epsilons_zoom[0], epsilons_zoom[-1]   #0, 2.1  # Epsilon range
+    v_min, v_max = VBs_zoom[0], VBs_zoom[-1] #-0.5, 0.5  # V range
+
+    inset_width = 0.21  # 30% of the parent axes
+    inset_height = 1.4*inset_width * (v_max - v_min) / (eps_max - eps_min)  # Maintain aspect ratio
+    axins = inset_axes(ax, width=f"{inset_width*100}%", height=f"{inset_height*100}%", loc="upper right")
+    img_zoom = axins.imshow(Y_zoom, extent=[eps_min, eps_max, v_min, v_max], aspect='auto', origin='lower', cmap='bwr')
+    axins.set_xticks([])  # Remove ticks for clarity
+    axins.set_yticks([])
+
+    # Mark the zoomed region with a rectangle
+    ax.plot([eps_min, eps_max, eps_max, eps_min, eps_min], [v_min, v_min, v_max, v_max, v_min], 'r-', linewidth=2)
+    # Connect main plot and inset with lines
+    mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="red")
+    plt.show()
+
+def plot_big_coulomb_report(epsilons, VBs, Y, title, epsilons_zoom, VBs_zoom, Y_zoom):
+
+    # Create Figure
+
+    #plt.figure(figsize=(8, 5))  # Optional: set figure size
+    #text_font = 18
+    #plt.rcParams.update({'font.size': 16})  # Applies globally
+    fig, ax = plt.subplots(figsize=(8, 5))
+
+    # Main imshow plot
+    img = ax.imshow(Y, extent=[epsilons[0], epsilons[-1], VBs[0], VBs[-1]], aspect='auto', origin='lower', cmap='bwr')
+    fig.colorbar(img, ax=ax)
+    ax.set_title(title)
+    ax.set_xlabel(r"$\epsilon$")
+    ax.set_ylabel('$V_B$')
+
+    # Define zoom area (e.g., focus on a small square region)
+    eps_min, eps_max = epsilons_zoom[0], epsilons_zoom[-1]   #0, 2.1  # Epsilon range
+    v_min, v_max = VBs_zoom[0], VBs_zoom[-1] #-0.5, 0.5  # V range
+
+    inset_width = 0.3  # 30% of the parent axes
+    inset_height = inset_width * (v_max - v_min) / (eps_max - eps_min)  # Maintain aspect ratio
+    axins = inset_axes(ax, width=f"{inset_width*100}%", height=f"{inset_height*100}%", loc="upper right")
+    img_zoom = axins.imshow(Y_zoom, extent=[eps_min, eps_max, v_min, v_max], aspect='auto', origin='lower', cmap='bwr')
+    axins.set_xticks([])  # Remove ticks for clarity
+    axins.set_yticks([])
+
+    # Mark the zoomed region with a rectangle
+    ax.plot([eps_min, eps_max, eps_max, eps_min, eps_min], [v_min, v_min, v_max, v_max, v_min], 'r-', linewidth=2)
+    # Connect main plot and inset with lines
+    mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="red")
     plt.show()
 
 def plot_zoomed_out(epsilons, VBs, Y1, title1, Y2, title2, epsilons_zoom, VBs_zoom, Y1_zoom, Y2_zoom, suptitle):
